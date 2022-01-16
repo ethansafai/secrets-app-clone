9258f62b454ffc80ef1092d424bddeb89490a9e6
Ethan Safai <ethan.safai@gmail.com>
2022-01-16 00:04:06 -0800

    Replace encryption with md5 hashing

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -19,10 +19,6 @@ const userSchema =  new mongoose.Schema({
   email: String,
   password: String
 });
-
-const secret = process.env.SECRET;
-
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -38,10 +34,14 @@ app.get("/register", (req, res) => {
   res.render("register");
 });
 
+app.get("/logout", (req, res) => {
+  res.redirect("/");
+})
+
 app.post("/register", (req, res) => {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(err => {
@@ -52,7 +52,7 @@ app.post("/register", (req, res) => {
 
 app.post("/login", (req, res) => {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username}, (err, foundUser) => {
     if (err) console.log(err);
